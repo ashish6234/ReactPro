09e524d909f81ec1bf46d81924e55bf8279a822b
ashish6234 <s.ashish62.aa@gmail.com>
2023-08-18 13:17:03 +0530

    V-1.1, Added Two project to achieve there resourses and the name is vite-project-one and react-project-one

---
+++ react-project-one/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h2>Project with React</h2>
+  );
+}
+
+export default App;
---
+++ vite-project-one/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h2>Project One of Vite and React</h2>
+  )
+}
+
+export default App
